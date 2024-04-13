f092ffbe632401c463d3b6e233d03f98b6e75edd
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-13 14:00:02 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log("Welcome to Ebook apis")
+import app from "./src/app";
+
+
+const startServer =  () => {
+    // Connect database
+  
+    const port = process.env.PORT || 3000;
+  
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
+  };
+  
+
+  startServer();
